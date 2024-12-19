70bcf217178757e4c37dfee2f65e9a5afdef5108
MiloVirus <camilovalencia.dev@gmail.com>
2024-12-19 15:54:26 -0500

    Authentication implemented, Profile auth endpoint added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,34 @@
-import { Controller, HttpCode, HttpStatus, Post, Body } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post, Body, UseGuards, Get, Request, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './sign-in.dto';
+import { AuthGuard } from './auth.guard';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService){}
+    constructor(private authService: AuthService) {}
 
     @HttpCode(HttpStatus.OK)
     @Post('login')
-    signIn(@Body() signInDto: SignInDto) {
-        return this.authService.signIn(signInDto);
+    async signIn(@Body() signInDto: SignInDto, @Res() res: Response) {
+        const { email, msg, token } = await this.authService.signIn(signInDto); // Esperar el resultado del servicio
+
+        res.cookie('access_token', token, {
+            httpOnly: true, 
+            secure: process.env.NODE_ENV === 'production', 
+            maxAge: 3600000, 
+        });
+
+        return res.json({
+            msg,
+            email,
+            token
+        });
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
+        return req.user;
     }
 }
