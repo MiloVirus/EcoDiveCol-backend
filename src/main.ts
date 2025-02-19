40ed9fd03ec41ba5609e3970812ed50be69bd2f9
Camilo Valencia <camilovalencia.dev@gmail.com>
2025-02-19 15:16:54 -0500

    Repo ready for deployment

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser'
+import * as cookieParser from 'cookie-parser';
 import * as dotenv from 'dotenv';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as serverless from 'serverless-http';
+
+dotenv.config();
+const expressApp = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  dotenv.config();
-  app.useGlobalPipes(new ValidationPipe())
-  app.enableCors({ origin: 'http://localhost:5173', credentials: true});
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173', credentials: true });
   app.use(cookieParser());
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.init(); 
 }
+
 bootstrap();
+
+
+export const handler = serverless(expressApp);
