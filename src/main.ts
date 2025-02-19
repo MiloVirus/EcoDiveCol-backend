4f47353b85fab9cd13ac1b28e2813bfc6ceb7a4a
Camilo Valencia <camilovalencia.dev@gmail.com>
2025-02-19 18:33:03 -0500

    Fixing deployment

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,10 @@ const server = express();  // Crea un servidor Express
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({ origin: 'http://localhost:5173', credentials: true });
+  app.enableCors({ 
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173', 
+    credentials: true 
+  });
   app.use(cookieParser());
   await app.init();  // NO usamos app.listen(), porque Vercel maneja el servidor
 }
