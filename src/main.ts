f064f4e1107117cc12cac0df84cba1cd8474a0f0
Camilo Valencia <camilovalencia.dev@gmail.com>
2025-02-19 19:13:10 -0500

    Local working again

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const server = express();  // Crea un servidor Express
+import * as cookieParser from 'cookie-parser'
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({ 
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173', 
-    credentials: true 
-  });
+  const app = await NestFactory.create(AppModule);
+  dotenv.config();
+  app.useGlobalPipes(new ValidationPipe())
+  app.enableCors({
+    origin: '*',
+  });  
   app.use(cookieParser());
-  await app.init();  // NO usamos app.listen(), porque Vercel maneja el servidor
+  await app.listen(process.env.PORT ?? 3000);
 }
-
 bootstrap();
-export default server;  // Exportamos el servidor para Vercel
