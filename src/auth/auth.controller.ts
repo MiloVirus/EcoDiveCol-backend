530d2efaeaedaa6df1247296a5be97679515418c
Camilo Valencia <camilovalencia.dev@gmail.com>
2025-02-20 16:11:57 -0500

    Samesite modified for cookie reception

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -21,8 +21,8 @@ export class AuthController {
 
         res.cookie('access_token', token, {
             httpOnly: true, 
-            secure: process.env.NODE_ENV === 'production', 
-            sameSite: 'lax',
+            secure: true,
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000, 
             path: '/', 
         });
